3817dbaf85dfabac9f6c64e7cc1b55fbcf967880
Ubuntu <ubuntu@ip-172-31-41-253.ec2.internal>
2017-11-24 21:40:41 +0000

    updated print statements to work with python 3

--- broth/__init__.py
+++ broth/__init__.py
@@ -14,10 +14,10 @@ class Broth:
 
         if soups:
             if errors:
-                print "[broth] We weren't able to parse with ", errors, "but don't worry we were able to parse with", [lst[2] for lst in soups]
+                print("[broth] We weren't able to parse with ", errors, "but don't worry we were able to parse with", [lst[2] for lst in soups])
             self.soup = sorted(soups, key=lambda tup: -1 * tup[1])[0][0]
         else:
-            print "[broth] We weren't able to parse with any parsers"
+            print("[broth] We weren't able to parse with any parsers")
 
     @property
     def tables(self):
